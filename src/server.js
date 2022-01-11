1847062d4c0be3817c1c3465e126bb9e266ede78
Etienne <etienne.guilhaume@sap.com>
2022-01-11 15:06:46 +0100

    cors issues with cookies

--- src/server.js
+++ src/server.js
@@ -8,29 +8,37 @@ const port = 3000;
 var usersRouter = require("./routes/users");
 var authRouter = require("./routes/google-auth");
 
-app.use(cors());
+var corsOptions = {
+  origin: "*",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(session({
-  secret: "SECRET",
-  cookie: { domain:'.herokuapp.com'},
-  resave: true,
-  saveUninitialized: true
-}));
+app.use(
+  session({
+    secret: "SECRET",
+    cookie: { domain: ".herokuapp.com" },
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 app.use("/users", usersRouter);
 app.use("/google-auth", authRouter);
 
 const uri = process.env.MONGODB_URI;
 
 var mongoose = require("mongoose");
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, dbName: 'fitdb' });
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: "fitdb",
+});
 //Get the default connection
 var db = mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
